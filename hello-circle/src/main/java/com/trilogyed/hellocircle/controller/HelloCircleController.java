1c0ebbc528893096de872eb1118358e34dfc0105
scouring pad <hcc780921@gmail.com>
2022-07-28 13:40:41 -0700

    controller test added

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -9,4 +9,13 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
+    }
 }
+
+
+
+
